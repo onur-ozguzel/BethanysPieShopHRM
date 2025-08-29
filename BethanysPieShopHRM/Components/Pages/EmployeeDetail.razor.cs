2bd07455c0c682831fbb0c5c08a9bfffc7351969
Onur Ozguzel <o.ozguzel@vanlanschotkempen.com>
2025-08-29 17:29:46 +0200

    End of Module 4

---
+++ BethanysPieShopHRM/Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShopHRM.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.Components.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; } = new Employee();
+
+        protected override void OnInitialized()
+        {
+            Employee = MockDataService.Employees.FirstOrDefault(e => e.EmployeeId == EmployeeId);
+        }
+
+        private void ChangeHolidayState()
+        {
+            Employee.IsOnHoliday = !Employee.IsOnHoliday;
+        }
+    }
+}
